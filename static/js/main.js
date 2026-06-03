763017711795ff1e5d212513ba1f3c39c2d422ad
S.M. Safi <smshafi2099@gmail.com>
2026-06-03 16:56:19 +0600

    Add Sepia, invert, and the ability to choose export format

--- static/js/main.js
+++ static/js/main.js
@@ -25,16 +25,50 @@ imageInput.addEventListener('change', function() {
 
 function greyScale(ctx, imageData) {
     const newImageData = ctx.createImageData(canvas.width, canvas.height);
-    const originalPixels = imageData.data;
+    const origPixels = imageData.data;
     const newPixels = newImageData.data;
 
-    for (let i = 0; i < originalPixels.length; i += 4) {
-        const average = ((originalPixels[i] + originalPixels[i + 1] + originalPixels[i + 2]) / 3) | 0;
+    for (let i = 0; i < origPixels.length; i += 4) {
+        const average = ((origPixels[i] + origPixels[i + 1] + origPixels[i + 2]) / 3) | 0;
 
         newPixels[i] = average;
         newPixels[i + 1] = average;
         newPixels[i + 2] = average;
-        newPixels[i + 3] = 255;
+        newPixels[i + 3] = origPixels[i + 3];
+    }
+
+    ctx.putImageData(newImageData, 0, 0);
+}
+
+function sepia(ctx, imageData) {
+    const newImageData = ctx.createImageData(canvas.width, canvas.height);
+    const origPixels = imageData.data;
+    const newPixels = newImageData.data;
+
+    for (let i = 0; i < origPixels.length; i += 4) {
+        const sepiaRed = Math.round(.393 * origPixels[i] + .769 * origPixels[i + 1] + .189 * origPixels[i + 2]);
+        const sepiaGreen = Math.round(.349 * origPixels[i] + .686 * origPixels[i + 1] + .168 * origPixels[i + 2]);
+        const sepiaBlue = Math.round(.272 * origPixels[i] + .534 * origPixels[i + 1] + .131 * origPixels[i + 2]);
+
+        newPixels[i] = sepiaRed;
+        newPixels[i + 1] = sepiaGreen;
+        newPixels[i + 2] = sepiaBlue;
+        newPixels[i + 3] = origPixels[i + 3];
+    }
+
+    ctx.putImageData(newImageData, 0, 0);
+}
+
+function invert(ctx, imageData) {
+    const newImageData = ctx.createImageData(canvas.width, canvas.height);
+    const origPixels = imageData.data;
+    const newPixels = newImageData.data;
+
+    for (let i = 0; i < origPixels.length; i += 4) {
+        newPixels[i] = 255 - origPixels[i];
+        newPixels[i + 1] = 255 - origPixels[i + 1];
+        newPixels[i + 2] = 255 - origPixels[i + 2];
+        newPixels[i + 3] = origPixels[i + 3];
     }
 
     ctx.putImageData(newImageData, 0, 0);
@@ -56,7 +90,27 @@ greyScaleButton.addEventListener('click', function() {
     else {
         console.log('image not loaded yet');
     }
-})
+});
+
+const sepiaButton = document.getElementById('sepia-button');
+sepiaButton.addEventListener('click', function() {
+    if (imageOK()) {
+        sepia(ctx, originalImageData);
+    }
+    else {
+        console.log('image not loaded yet');
+    }
+});
+
+const invertButton = document.getElementById('invert-button');
+invertButton.addEventListener('click', function() {
+    if (imageOK) {
+        invert(ctx, originalImageData);
+    }
+    else {
+        console.log('image not loaded yet');
+    }
+});
 
 const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', function() {
@@ -66,17 +120,21 @@ resetButton.addEventListener('click', function() {
     else {
         console.log('image not loaded yet');
     }
-})
+});
 
 const exportButton = document.getElementById('export-button');
+const exportDropdown = document.getElementById('export-dropdown');
 exportButton.addEventListener('click', function() {
-    if (imageOK()) {
+    if (imageOK() && exportDropdown.value) {
         canvas.toBlob(function(blob) {
             const url = URL.createObjectURL(blob);
             const link = document.createElement('a');
-            link.download = "file.png";
+            link.download = "image";
             link.href = url;
             link.click();
-        })
+        }, exportDropdown.value);
     }
-})
+    else {
+        console.log('image not loaded yet');
+    }
+});
