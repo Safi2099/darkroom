ca1d10660c7fa31fed3ee029dcb866d53ad143cd
S.M. Safi <smshafi2099@gmail.com>
2026-07-08 12:12:00 +0600

    Add rudimentary crop (no UI yet)

--- static/js/transforms.js
+++ static/js/transforms.js
@@ -101,7 +101,30 @@ export function resize(ctx, newWidth, newHeight) {
     ctx.putImageData(newImageData, 0, 0);
 }
 
-export function crop(ctx) {
+export function crop(ctx, cropX, cropY, width, height) {
+    const oldImageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const oldData = oldImageData.data;
 
+    const newImageData = ctx.createImageData(width, height);
+    const newData = newImageData.data;
+
+    for (let i = 0; i < newData.length; i += 4) {
+        const pixelIndex = i / 4;
+        const newX = pixelIndex % width;
+        const newY = (pixelIndex / width) | 0;
+
+        const oldX = cropX + newX;
+        const oldY = cropY + newY;
+        const oldIndex = (oldY * oldImageData.width + oldX) * 4;
+
+        newData[i] = oldData[oldIndex];
+        newData[i + 1] = oldData[oldIndex + 1];
+        newData[i + 2] = oldData[oldIndex + 2];
+        newData[i + 3] = oldData[oldIndex + 3];
+    }
+
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    ctx.putImageData(newImageData, 0, 0);
 }
 
